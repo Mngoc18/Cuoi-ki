e31f8e6da4dac80fa768f08b52735782c0aa6085
Mngoc18 <vtmngoc1810@gmail.com>
2024-12-14 21:56:23 +0700

    Add files via upload

---
+++ gioithieu.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html class="no-js" lang="vi">
+    <head>
+        <meta charset="utf-8"/>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+        <meta name="revisit-after" content="1 day"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="HandheldFriendly" content="true">
+        <link rel="shortcut icon" href="./logo.jpg" type="image/png"/>
+        <title> Giới thiệu - LUNASHOES</title>
+        <meta name="robots" content="index,follow,noodp">
+        <meta http-equiv="x-dns-prefetch-control" content="on">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet">
+        <script defer src='https://stats.hstatic.net/beacon.min.js' hrv-beacon-t='1000378787'></script>
+        <link rel="stylesheet" href="gioithieu.css">
+        <link href='//theme.hstatic.net/1000378787/1000485984/14/styles.css?v=348' rel='stylesheet' type='text/css' media='all'/>
+        <script src='//theme.hstatic.net/1000378787/1000485984/14/jquery.min.1.11.0.js?v=348' type='text/javascript'></script>
+        
+    </head>
+
+    <?php include('header.php'); ?>
+        
+            <div class="row wrapper-row pd-page">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="box_image visible-lg visible-md">
+                    <div class="banner">
+                        <a href="./trangchu.index.html">
+                            <img src="./Banner.png" alt="LUNASHOES">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="page-wrapper">
+                <div class="heading-page">
+                    <h1>Giới thiệu</h1>
+                </div>
+                <div class="wrapbox-content-page">
+                    <div class="content-page ">
+                        <p class="subtitle">LUNASHOES - Gửi Gắm Phong Cách Trong Từng Bước Chân</p>
+                        <h2>Giày Dép Nữ Hàng Đầu Việt Nam</h2>
+                        <p>Mỗi bước chân là một câu chuyện và LUNASHOES muốn giúp bạn kể câu chuyện ấy theo cách thật đặc biệt.</p>
+                        <p>Chúng tôi tự hào là thương hiệu giày luôn cập nhật những xu hướng mới nhất và làm hài lòng khách hàng để có những trải nghiệm mua sắm tốt nhất.</p>
+                        <p>Hãy đến với Lunashoes để tìm cho mình những đôi giày không chỉ đẹp mà còn "nói lên" phong cách của bạn!.</p> 
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!--THÀNH VIÊN NHÓM-->
+        <div class="team-container">
+            <div class="team-member">
+                <img src="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/418510170_1403189433911996_3672413764900216467_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeG3vQQIw_0_8dLAaLWA39PekGxub_pjQjaQbG5v-mNCNmbEqif1m1f8xqjuHaMnbwg5IHQ3-BXnF4CzDNTjeH6E&_nc_ohc=ui7mktI734UQ7kNvgGL4lEB&_nc_zt=23&_nc_ht=scontent.fdad3-5.fna&_nc_gid=Aqs611fmALYEvZCiscD9U0L&oh=00_AYA5lm953zRdxq86kM15kdKiWSvAZdYCY08fc3Q4S_kfxA&oe=675E3356" alt="Thành viên 1" class="team-photo">
+                <p class="team-name">Lương Thị Mỹ Duyên</p>
+            </div>
+            <div class="team-member">
+                <img src="path/to/member2.jpg" alt="Thành viên 2" class="team-photo">
+                <p class="team-name">Nguyễn Đỗ Quang Hưng</p>
+            </div>
+            <div class="team-member">
+                <img src="" alt="Thành viên 3" class="team-photo">
+                <p class="team-name">Nguyễn Ngọc Xuân Nam</p>
+            </div>
+            <div class="team-member">
+                <img src="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/450923785_1887157121786455_8315611583921274576_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeH_R3iJvmikHioAJpRnJeu5WL-nysAdqjVYv6fKwB2qNQXjNYfGNavbDmkZWfJlE4_89WCvhBEouOxQEF1B62LE&_nc_ohc=nm4g-7gPzUoQ7kNvgHKNTW2&_nc_zt=23&_nc_ht=scontent.fdad3-5.fna&_nc_gid=A9soJpIE33W8-x5B32YCUwJ&oh=00_AYCg9GG27y7Mo40clZroaoBhQAvrDyhfksGlY-8Ze5girg&oe=675E1F5F" alt="Thành viên 4" class="team-photo">
+                <p class="team-name">Võ Thị Mỹ Ngọc</p>
+            </div>
+        </div>
+        
+      <?php include('footer.php'); ?>
